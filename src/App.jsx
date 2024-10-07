a0249f8fca3ad3f9d417cceb841b41c68eb4b855
DawidstudentKUL <73649090+zalewskidawid@users.noreply.github.com>
2024-10-07 22:49:32 +0200

    Render components conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject></NewProject>;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar></ProjectsSidebar>
-      <NoProjectSelected></NoProjectSelected>
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+      ></ProjectsSidebar>
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button";
-function NoProjectSelected() {
+function NoProjectSelected({onStartAddProject}) {
   return (
     <div className="mt-2 text-center w-2/3">
       <img
@@ -16,7 +16,7 @@ function NoProjectSelected() {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
